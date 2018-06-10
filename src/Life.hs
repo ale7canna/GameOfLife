84e598272fcb800480e34376e7cd244f572c1bc3
Alessandro Canicatti <ales.canicatti@gmail.com>
2018-06-10 12:47:47 +0200

    Tests will be discovered from Hspec

--- src/Life.hs
+++ src/Life.hs
@@ -3,8 +3,15 @@ module Life where
 data Cell = Cell Int Int
   deriving (Show, Eq)
 
+class CellCapable a where
+  cell :: a -> Cell
+
 data Life = Alive Cell | Dead Cell
   deriving (Show, Eq)
+
+instance CellCapable Life where
+  cell (Alive c) = c
+  cell (Dead c)  = c
 
 createLife :: Cell -> Life
 createLife c = Alive c
@@ -15,13 +22,8 @@ createDead c = Dead c
 neighbours :: Life -> [Life] -> [Life]
 neighbours _ [] = []
 neighbours life others =
-  filter (neigh life) others
-
-neigh :: Life -> Life -> Bool
-neigh (Alive c1) (Alive c2) = isNear c1 c2
-neigh (Alive c1) (Dead c2)  = isNear c1 c2
-neigh (Dead c1)  (Alive c2) = isNear c1 c2
-neigh (Dead c1)  (Dead c2)  = isNear c1 c2
+  filter (areNeighbours life) others
+  where areNeighbours = \c1 c2 -> isNear (cell (c1)) (cell (c2))
 
 isNear :: Cell -> Cell -> Bool
 isNear (Cell r1 c1) (Cell r2 c2) = abs(r2 - r1) <= 1 && abs (c2 - c1) <= 1
